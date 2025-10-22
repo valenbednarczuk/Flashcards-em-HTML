724e875b77e2c245710bc0bf86a8d38706d040de
valenbednarczuk <valentina.deus@escola.pr.gov.br>
2025-10-22 08:50:52 -0300

    v

---
+++ assets/app.js
@@ -0,0 +1,4 @@
+function criaCartao(categoria, pergunta, ressposta){
+    let container= document.getElementById('container')
+    let cartao= document.createElement('article')
+    cartao.className= 'cartao'
