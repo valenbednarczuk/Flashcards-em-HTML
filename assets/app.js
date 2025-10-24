356f07b3d3a8a9a3718b51e4993fbc5973ddf6f1
valenbednarczuk <valentina.deus@escola.pr.gov.br>
2025-10-24 10:45:11 -0300

    v

--- assets/app.js
+++ assets/app.js
@@ -2,3 +2,16 @@ function criaCartao(categoria, pergunta, ressposta){
     let container= document.getElementById('container')
     let cartao= document.createElement('article')
     cartao.className= 'cartao'
+
+    cartao.innerHTML= `
+    <div class= <"cartao_conteudo">
+    <h3>${categoria}</h3>
+    <div class="cartao_conteudo_pergunta">
+    <p>${pergunta}</p>
+    </div>
+    <div class="cartao_conteudo_resposta">
+    <p>${resposta}</p>
+    </div>
+    </div>
+
+    
