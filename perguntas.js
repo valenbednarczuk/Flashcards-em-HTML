a522e824179894f0fd65831b52e5ad28b6bd7a6a
valenbednarczuk <valentina.deus@escola.pr.gov.br>
2025-11-12 11:20:19 -0300

    12 nov de 2025

--- perguntas.js
+++ perguntas.js
@@ -1,25 +1,26 @@
 criaCartao(
-    'Programação'
-    'O que é Python'
-    'O Python é uma linguagem de programação'
-)
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação.'
+);
 
 criaCartao(
     'Geografia',
     'Qual é a capital da França?',
-    'A capital da França é Paris',
-)
+    'A capital da França é Paris.'
+);
 
 criaCartao(
     'Programação',
-    'O que é uma função?'
-    'Uma função é um bloco de código que executa alguma tarefa'
-)
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa uma tarefa.'
+);
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em inglês é Hi (RAI)'
-)
+    'Língua Inglesa',
+    'Como se diz oi em inglês?',
+    'Oi em inglês é "Hi" (pronuncia-se RAI).'
+);
+
 
 
