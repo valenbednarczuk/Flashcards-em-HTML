45085a11b816def67b61f4e86797e8f12190078d
valenbednarczuk <valentina.deus@escola.pr.gov.br>
2025-11-19 08:07:39 -0300

    19 nov 2025 2

--- perguntas.js
+++ perguntas.js
@@ -1,27 +1,26 @@
-function criaCartao(categoria, pergunta, resposta) {
-    let container = document.getElementById('container');
-    let cartao = document.createElement('article');
-    cartao.className = 'cartao';
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
 
-    cartao.innerHTML = `
-        <div class="cartao__conteudo">
-            <h3>${categoria}</h3>
-            <div class="cartao__conteudo__pergunta">
-                <p>${pergunta}</p>
-            </div>
-            <div class="cartao__conteudo__resposta">
-                <p>${resposta}</p>
-            </div>
-        </div>
-    `;
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
 
-    cartao.addEventListener('click', () => {
-        cartao.classList.toggle('active');
-    });
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
 
-    container.appendChild(cartao);
-}
-
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
 
 
 
